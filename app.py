eb56d59b9ec1a6afa8dfcc9250ddaf1859f42e65
ayavuzer-hacken <a.yavuzer@hacken.io>
2024-05-08 12:52:36 +0300

    feat: portal adjustment

--- app.py
+++ app.py
@@ -4,38 +4,47 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/calculate")
+@app.route("/calculate", methods=['POST'])
 def calculate():
-    likelihood = int(request.args.get('L'))
-    impact = int(request.args.get('I'))
-    exploitability = int(request.args.get('E'))
-    complexity = int(request.args.get('C'))
 
-    if likelihood not in range(1, 6):
-        return f"invalid likelihood: {likelihood}", 400
-    elif impact not in range(1, 6):
-        return f"invalid impact: {impact}", 400
-    elif exploitability not in range(3):
-        return f"invalid exploitability: {exploitability}", 400
-    elif complexity not in range(3):
-        return f"invalid issue_complexity: {complexity}", 400
+    if request.is_json:
+        data = request.json
+        likelihood = int(data["Likelihood"])
+        impact = int(data["Impact"])
 
-    (result, _, _, _, _,) = calculate_severity(
-        likelihood=likelihood, 
-        impact=impact,
-        exploitability=exploitability,
-        issue_complexity=complexity
-    )
+        exploitability = int(search_item_by_name("Dropdown1", data)["Value"]) - 1 # custom fields start from 1 rather than 0
+        complexity = int(search_item_by_name("Dropdown2", data)["Value"]) - 1 # custom fields start from 1 rather than 0
 
-    (label, _) = generate_label(result)
-    score = "{:.1f}".format(result)
+        if likelihood not in range(1, 6):
+            return f"invalid likelihood: {likelihood}", 400
+        elif impact not in range(1, 6):
+            return f"invalid impact: {impact}", 400
+        elif exploitability not in range(3):
+            return f"invalid exploitability: {exploitability}", 400
+        elif complexity not in range(3):
+            return f"invalid issue_complexity: {complexity}", 400
+
+        (result, _, _, _, _,) = calculate_severity(
+            likelihood=likelihood, 
+            impact=impact,
+            exploitability=exploitability,
+            issue_complexity=complexity
+        )
+
+        (label, _) = generate_label(result)
+        result = round(result, 1)
+        data = {"result": result,  "severity": label, "version": VERSION}
     
-    data = {"score": score,  "severity": label, "version": VERSION}
-    
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
+        response = app.response_class(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
 
-    return response
+        return response
+
+def search_item_by_name(name, data):
+    for item in data['CustomFields']:
+        if item['Name'] == name:
+            return item
+    return None
